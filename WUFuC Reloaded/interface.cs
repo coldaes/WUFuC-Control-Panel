085b932ee4577b3ee31896cc58268930b957406e
LCN <LCN-01>
2022-01-24 01:39:35 +0100

    deleted unused using-directives

--- WUFuC Reloaded/interface.cs
+++ WUFuC Reloaded/interface.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Diagnostics;
 using System.Windows.Forms;
-using System.Reflection;
 
 namespace WUFuC_Reloaded
 {
